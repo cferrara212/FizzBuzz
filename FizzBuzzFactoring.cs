9ba4356e2eb32678e9e1cca0b7e44170732efd17
unknown <cferrara212@gmail.com>
2021-11-16 11:57:07 -0700

    FizzBuzz commit

---
+++ FizzBuzzFactoring.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+       {
+
+        FizzBuzz fizz = new FizzBuzz();
+
+            fizz.RunFizzBuzz();
+            Console.ReadLine();
+        
+       }
+
+
+     }
+
+       
+
+    
+    }
+
